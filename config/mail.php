52950ef208b7e183385817592c11d62729e28ced
Archie Quito <archie@ideatesystems.com>
2015-07-14 09:13:39 +0800

    update mail config - test

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'archie@ideatesystems.com', 'name' => 'Admin'],
 
 	/*
 	|--------------------------------------------------------------------------
