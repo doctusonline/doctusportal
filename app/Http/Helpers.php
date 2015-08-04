9cc5cdb305381d4b3ef9114f391f88e78a76afde
Chikito <archie@ideatesystems.com>
2015-08-04 16:28:10 +0800

    add helpers file

---
+++ app/Http/Helpers.php
@@ -0,0 +1,18 @@
+<?php
+function format_interval($datetime = "2015-04-21 8:00:00") {
+    //$timezone = new DateTimeZone('Asia/Manila');
+    $first_date = new \DateTime();
+    $second_date = new \DateTime($datetime);
+    //$first_date->setTimezone($timezone);
+    $interval = $first_date->diff($second_date);
+    $result = "";
+    if ($interval->y) { $result .= $interval->format(($interval->y == 1) ? '%y year':'%y years'); }
+    elseif ($interval->m) { $result .= $interval->format(($interval->m == 1) ? '%m month':'%m months'); }
+    elseif ($interval->d) { $result .= $interval->format(($interval->d == 1) ? '%d day':'%d days'); }
+    elseif ($interval->h) { $result .= $interval->format(($interval->h == 1) ? '%h hour':'%h hours'); }
+    elseif ($interval->i) { $result .= $interval->format(($interval->i == 1) ? '%i minute':'%i minutes'); }
+    elseif ($interval->s) { $result .= $interval->format(($interval->s == 1) ? '%s second':'%s seconds'); }
+
+    return $result;
+}
+?>
